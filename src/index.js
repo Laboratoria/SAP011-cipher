191e7160220cf8876d2707de8cbb6cf8d32678fa
belatenorio <117682146+belatenorio@users.noreply.github.com>
2023-07-18 19:39:06 -0300

    alterações no css e index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+//Passando valores de um campo input para outro input
+
+//Cifrar
+
+const bottonSubmit = document.querySelector("#submit");
+
+bottonSubmit.addEventListener("click", function (e) {
+
+    e.preventDefault(); //paralisa o envio do formulário 
+
+    const conteudo = document.querySelector("conteudo");
+    const cifrar = documento.querySelector("cifrar");
+
+    cifrar.value = conteudo.value
+})
+
+//Decifrar
+
+const bottonSend = document.querySelector("#send");
+
+bottonSend.addEventListener("click", function (e) {
+
+    e.preventDefault(); //paralisa o envio do formulário 
+
+    const senha = document.querySelector("#senha");
+    const decifrar = document.querySelector("#decifrar");
+
+    decifrar.value = senha.value;
+    //const value = senha.value;
+
+    //console.log(value);
+
+})
