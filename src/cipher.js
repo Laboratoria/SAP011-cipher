f932c21b64dee146a45aa4333232386cdf7da355
belatenorio <117682146+belatenorio@users.noreply.github.com>
2023-07-28 17:05:17 -0300

    criação da lógica caesar cipher, mudança de tema

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,43 @@
+//dentro da constante cipher criar objeto encode e decode
 const cipher = {
-  // ...
-};
+  encode: function (deslocamento, conteudo) {
+    let textoCodificado = ""
+    for (let i = 0; i < conteudo.length; i++) {
+      const codigoASC = conteudo.charCodeAt(i);
+      // Verifica se o caractere é uma letra maiúscula (códigos ASCII 65 a 90).
+      if (codigoASC >= 65 && codigoASC <= 90) {
+        //Aplica o deslocamento para codificar o caractere.
+        const codigoCodificado = (codigoASC - 65 + deslocamento) % 26 + 65
+        //Converte o código ASCII de volta para o caractere correspondente.
+        const caractereCodificado = String.fromCharCode(codigoCodificado)
+        // Adiciona o caractere codificado na string final.
+        textoCodificado += caractereCodificado 
+      } else {
+        // Caso o caractere não seja uma letra maiúscula, mantém o caractere inalterado na string final.
+        textoCodificado += conteudo[i] 
+        //colocar um alert
+      }
+    }
+    return textoCodificado // Retorno da string final codificada.
+  }, 
+  decode: function (deslocDecode, message) {
+    let textoDecodificado = ""
+    for (let i = 0; i < message.length; i++) {
+      const ascCodigo = message.charCodeAt(i);
 
+      if (ascCodigo >= 65 && ascCodigo <= 90) {
+        // Aplica o deslocamento negativo para decodificar o caractere.
+        const codigoDecodificado = (ascCodigo + 65 + deslocDecode) % 26 + 65
+        const caractereDecodificado = String.fromCharCode(codigoDecodificado)
+        // Adiciona o caractere decodificado na string final.
+        textoDecodificado += caractereDecodificado
+      }else{
+        textoDecodificado += message[i]
+        //colocar um alert
+      }
+    }
+    return textoDecodificado // Retorno a string final decodificada.
+  }
+};  
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -4,34 +4,39 @@ console.log(cipher);
 
 //Passando valores de um campo input para outro input
 
-//Cifrar
+//Cifrar ids: conteudo, cifrar, submit, number
 
 const bottonSubmit = document.querySelector("#submit");
 
 bottonSubmit.addEventListener("click", function (e) {
 
-    e.preventDefault(); //paralisa o envio do formulário 
+  e.preventDefault(); //paralisa o envio do formulário
 
-    const conteudo = document.querySelector("conteudo");
-    const cifrar = documento.querySelector("cifrar");
+  const conteudo = document.querySelector("#conteudo")
+  const cifrar = document.querySelector("#cifrar")
+  const deslocamento = document.querySelector("#number")
+  const mensagemCodificada = cipher.encode(Number(deslocamento.value), conteudo.value.toUpperCase())
 
-    cifrar.value = conteudo.value
+  cifrar.value = mensagemCodificada
 })
 
-//Decifrar
+// Decifrar ids: message, decifrar, send, desloc
 
-const bottonSend = document.querySelector("#send");
+const bottonSend = document.querySelector("#send")
 
 bottonSend.addEventListener("click", function (e) {
 
-    e.preventDefault(); //paralisa o envio do formulário 
+  e.preventDefault() //paralisa o envio do formulário 
 
-    const senha = document.querySelector("#senha");
-    const decifrar = document.querySelector("#decifrar");
+  const message = document.querySelector("#message")
+  const decifrar = document.querySelector("#decifrar")
+  const deslocDecode = document.querySelector("#desloc")
+  const mensagemDecodificada = cipher.decode(Number(deslocDecode.value), message.value.toUpperCase())
 
-    decifrar.value = senha.value;
-    //const value = senha.value;
-
-    //console.log(value);
-
+  decifrar.value = mensagemDecodificada //letras minúsculas em maiúsculas
 })
+
+
+
+
+
